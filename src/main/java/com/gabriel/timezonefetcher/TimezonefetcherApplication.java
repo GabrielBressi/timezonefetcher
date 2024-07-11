4ae2b580ae3934eeb42d8cc28142fbe606e89440
Gabriel Bressi <gabriel@C11-I5BLCMJN31J.dir.svc.accenture.com>
2024-07-11 11:37:19 -0300

    first commit

---
+++ src/main/java/com/gabriel/timezonefetcher/TimezonefetcherApplication.java
@@ -0,0 +1,13 @@
+package com.gabriel.timezonefetcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimezonefetcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimezonefetcherApplication.class, args);
+	}
+
+}
